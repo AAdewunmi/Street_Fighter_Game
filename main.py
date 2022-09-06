b330fc2e7806a796876a9666c8cf590b7e87fc24
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-09-06 08:32:40 +0100

    Six Commit: Implement Function for drawing background

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ pygame.init()
 # Create Game Window
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Street Fighter!")
+
+# Load Background Image
+bg_image = pygame.image.load("assets/images/background.jpg").convert_alpha()
+
+
+# Function For Drawing Background
+def draw_bg():
+    screen.blits(bg_image, (0, 0))
+
 
 # Create Game Loop
 run = True
