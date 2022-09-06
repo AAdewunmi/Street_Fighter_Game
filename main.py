e299f6fd23bb47f50b9371192dd1992592e4924c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-09-06 08:14:02 +0100

    Three Commit: Implement Game Window

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import pygame
 
 # Initialise Pygame
 pygame.init()
+
+# Create Game Window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Street Fighter!")
