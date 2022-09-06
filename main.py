c3e496cdc2559f140f37515678830061b9f8c721
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-09-06 08:21:28 +0100

    Four Commit: Implement Skeleton Game Loop

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Street Fighter!")
+
+# Create Game Loop
+run = True
+while run:
+
+    # Implement Event Handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+# Exit PyGame
+pygame.quit()
