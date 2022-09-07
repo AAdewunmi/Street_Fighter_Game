325ef08bd0d58b19ef610318c7088bfe4d3208f9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-09-07 07:20:52 +0100

    Twelve Commit: Implement Skeleton draw() method

--- fighter.py
+++ fighter.py
@@ -5,3 +5,6 @@ import pygame
 class Fighter():
     def __init__(self, x: object, y: object) -> object:
         self.rect = pygame.Rect((x, y, 80, 180))
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 0, 0), self.rect)
