f1e881df1672cdeaa8bf5d5fc38b2c0e0ce6cf21
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-09-09 12:51:14 +0100

    Twenty Commit: Implement functionality to restrict player movement with screen.

--- fighter.py
+++ fighter.py
@@ -6,8 +6,7 @@ class Fighter():
     def __init__(self, x: object, y: object) -> object:
         self.rect = pygame.Rect((x, y, 80, 180))
 
-
-    def move(self):
+    def move(self, screen_width):
         SPEED = 10
         dx = 0
         dy = 0
@@ -20,6 +19,12 @@ class Fighter():
             dx = -SPEED
         if key[pygame.K_d]:
             dx = SPEED
+
+        # Ensure Player Stays On Screen
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > screen_width:
+            dx = screen_width - self.rect.right
         # Update Player Position
         self.rect.x += dx
         self.rect.y += dy
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Space Invaders Game
+# By: Adrian Adewunmi
+# Date: 28/07/2022
+# Version: 1.0
+# Description: This is the main file for the Space Invaders game.
+#             It contains the main game loop and the main game functions.
+#             It also contains the main menu and the game over screen.
+#             It also contains the high score screen.
+#             It also contains the pause screen.
+#             It also contains the game over screen.
+# Adapted from: https://www.youtube.com/watch?v=FfWpgLFMI7w
+#               https://github.com/attreyabhatt/Space-Invaders-Pygame
+
+
 import pygame
 from fighter import Fighter
 
@@ -35,8 +49,8 @@ while run:
     # Draw Background
     draw_bg()
     # Move Fighters
-    fighter_1.move()
-    fighter_2.move()
+    fighter_1.move(SCREEN_WIDTH)
+    # fighter_2.move()
     # Draw Fighters
     fighter_1.draw(screen)
     fighter_2.draw(screen)
