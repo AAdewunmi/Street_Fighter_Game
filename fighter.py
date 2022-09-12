fd2ef67963701a15e6aa2525393634b979d6ffee
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-09-12 16:20:49 +0100

    Twenty-Nine Commit: Implement TEST CASE to ensure that keystroke pressed will cause fighter_1 to hit fighter_2.

--- fighter.py
+++ fighter.py
@@ -20,7 +20,7 @@ class Fighter():
         self.jump = False
         self.attack_type = 0
 
-    def move(self, screen_width, screen_height, surface):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         dx = 0
@@ -42,7 +42,7 @@ class Fighter():
 
         # Player Attacking
         if key[pygame.K_r] or key[pygame.K_t]:
-            self.attack(surface)
+            self.attack(surface, target)
             # Determine which attack type was used
             if key[pygame.K_r]:
                 self.attack_type = 1
@@ -67,8 +67,10 @@ class Fighter():
         self.rect.x += dx
         self.rect.y += dy
 
-    def attack(self, surface):
+    def attack(self, surface, target):
         attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            print("HIT!")
         pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def draw(self, surface):
